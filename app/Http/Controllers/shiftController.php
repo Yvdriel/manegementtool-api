4786926d0dae81ea922eea8491b2efa496f08eb7
yvdriel <yoranvd2001@gmail.com>
2021-07-31 12:12:54 +0200

    - init - added shiftController - added shift Model

---
+++ app/Http/Controllers/shiftController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\shift;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class shiftController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return array
+     */
+    public function update(Request $request, int $id): array
+    {
+        return [$request->all(), $id];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
